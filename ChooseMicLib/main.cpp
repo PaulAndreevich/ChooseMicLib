7e00b8d653ed540fa09e965e0babab21efc29d66
Neon <abanshin@gmail.com>
2018-01-30 12:17:18 +0300

    Initial Commit

---
+++ ChooseMicLib/main.cpp
@@ -0,0 +1,15 @@
+//
+//  main.cpp
+//  ChooseMicLib
+//
+//  Created by Boss on 30/01/2018.
+//  Copyright © 2018 Boss. All rights reserved.
+//
+
+#include <iostream>
+
+int main(int argc, const char * argv[]) {
+    // insert code here...
+    std::cout << "Hello, World!\n";
+    return 0;
+}
