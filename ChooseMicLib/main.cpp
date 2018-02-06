1ad96c5fc6ad17b15346b8b14d370393b3cf3bcf
Neon <abanshin@gmail.com>
2018-02-06 21:23:02 +0300

    Committed first

--- ChooseMicLib/main.cpp
+++ ChooseMicLib/main.cpp
@@ -7,9 +7,37 @@
 //
 
 #include <iostream>
+#include "switch.hpp"
 
 int main(int argc, const char * argv[]) {
-    // insert code here...
-    std::cout << "Hello, World!\n";
+    
+    ChooseMic test;
+    test.findAllInputDevices(kAudioTypeInput);
+    std::string newDevice;
+    
+    test.initializeDeviceIterator();
+    //test.advanceDeviceIterator();
+    if (test.iteratorEnded() != true) newDevice = test.value();
+    
+    std::cout << "\nAll Devices:\n";
+    test.initializeDeviceIterator();
+    while(test.iteratorEnded() != true){
+        std::cout << test.value();
+        std::cout << "\n";
+        test.advanceDeviceIterator();
+    }
+
+    std::cout << "\nCurrent:\n";
+    std::cout << test.getCurrentInputDevice();
+    
+    std::cout << "\n\nWanted Device:\n";
+    std::cout << newDevice;
+    
+    test.setDevice((char*)newDevice.c_str());
+    
+    std::cout << "\n\nCurrent:\n";
+    std::cout << test.getCurrentInputDevice();
+    std::cout << "\n";
+    
     return 0;
 }
